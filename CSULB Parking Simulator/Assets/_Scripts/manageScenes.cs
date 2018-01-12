01d54ea9e5b75c9c4d04715e6c788678f1b0a706
nightsharks\Kei Matsuo <matsuokei.chan@gmail.com>
2018-01-12 10:59:52 -0800

    no message

---
+++ CSULB Parking Simulator/Assets/_Scripts/manageScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class manageScenes : MonoBehaviour {
+
+    public void startClick()
+    {
+        SceneManager.LoadScene(sceneName: "MainLevel");
+    }
+
+    public void quitClick()
+    {
+        Application.Quit();
+    }
+}
