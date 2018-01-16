eeb1b0263ce2f73ea5382fff8bd27b86eb377fae
nightsharks\Kei Matsuo <matsuokei.chan@gmail.com>
2018-01-15 20:30:45 -0800

    no message

--- CSULB Parking Simulator/Assets/_Scripts/PlayerController.cs
+++ CSULB Parking Simulator/Assets/_Scripts/PlayerController.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-
+    public Boundary boundary;
+    public float rotation;
     private Rigidbody rb;
+    public float velocity;
+    public float RotateSpeed;
 
     void Start()
     {
@@ -20,6 +29,23 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.velocity = movement * speed;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        }
+
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
     }
 }
